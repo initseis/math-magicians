15e9d90edc14ec47da069772fa403b70ff867cfa
Kevin <kevinsevinche@gmail.com>
2021-08-17 10:32:43 -0500

    Add Routes and Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
-import './App.css';
-import Calculator from './components/Calculator';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Calculator from "./components/Calculator";
+import Quote from "./components/Quote";
 
 class App extends React.Component {
   render() {
-    return <Calculator />;
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/calculator">Calculator</Link>
+              </li>
+              <li>
+                <Link to="/quote">Quote</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route exact path="/">
+              <Home />;
+            </Route>
+            <Route path="/calculator">
+              <Calculator />;
+            </Route>
+            <Route path="/quote">
+              <Quote />;
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
